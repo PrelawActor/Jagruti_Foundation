891677c2bac9c31e402ddf78517e4e18330ade64
PrelawActor <zaleshsalaskar@gmail.com>
2026-07-24 11:11:34 +0530

    ADDINGALL

--- Jagruti/src/App.jsx
+++ Jagruti/src/App.jsx
@@ -4,11 +4,23 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import CustomNavbar from "./assets/pages/Navbars.jsx";
 import './App.css'
+import Hero from "./assets/pages/Donation/Hero.jsx"
+import Paragraph from './assets/pages/Donation/Paragraph.jsx';
+import Card1 from './assets/pages/Donation/Card1.jsx';
+import Card2 from './assets/pages/Donation/Card2.jsx';
+import Donation from './assets/pages/Donation/Donation.jsx'
+import Cards3 from './assets/pages/Donation/Cards3.jsx'
 
 function App() {
   return (
     <>
       <CustomNavbar/ >
+      <Hero/>
+      <Paragraph/>
+      <Card1/>
+      <Card2/>
+      <Donation />
+      <Cards3 />
     </>
   )
 }
